e3edb63929cba77abcdc76130b606e89597c7cd5
nzm798 <798736800@qq.com>
2023-03-29 15:06:10 +0800

    ss-RPC-framework 1.0-SNAPSHOT

--- example-client/src/main/java/com/ss/NettyClientMain.java
+++ example-client/src/main/java/com/ss/NettyClientMain.java
@@ -1,8 +1,6 @@
 package com.ss;
 
 import com.ss.annotation.RpcScan;
-import com.ss.remoting.transport.netty.client.NettyRpcClient;
-import com.ss.remoting.transport.netty.server.NettyRpcServer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @RpcScan(basePackage = "com.ss")
